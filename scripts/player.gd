3249480fc0d75a17d20062cde34fe9b816aee36f
Alireza <alirezarm98@outlook.com>
2024-08-04 16:00:31 +0200

    Added a map and a pet

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,6 +16,8 @@ func player_movement(delta):
 		play_anim(1)
 		velocity.x = SPEED
 		velocity.y = 0
+	if Input.is_action_just_released("ui_right"):
+		velocity.x = 0
 	elif Input.is_action_pressed("ui_left"):
 		current_dir = "left"
 		play_anim(1)
